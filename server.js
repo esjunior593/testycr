3e11b41c8a79f7e8a28e7342f6d2a96ce29e09e9
esjunior593 <esjunior593@gmail.com>
2025-02-05 23:14:34 -0500

    apy37

--- server.js
+++ server.js
@@ -31,9 +31,7 @@ db.connect(err => {
 
 
 function extraerDatosOCR(text) {
-    const comprobanteRegex = /(?:No\.?\s*Comprobante|No\.?\s*|Comprobante(?:\s*Nro\.?)?|Número de transacción|Código de transacción|Referencia|N°)\s*[:#-]*\s*([A-Z0-9]+(?:[.-][A-Z0-9]+)*)/i;
-
-
+    const comprobanteRegex = /(?:Comprobante(?:\s*Nro\.?)?|Número de transacción|Código de transacción|Referencia|N°|No\.?)\s*[:#-]*\s*([A-Z0-9.-]{6,})/i;
     const nombresRegex = /(?:Para:|Beneficiario:|Perteneciente a:|Nombre:|Titular Cuenta:)\s*([A-Za-z\s]+)/i;
     const montoRegex = /\$?\s?(\d+[\.,]\d{2})/i;
 
