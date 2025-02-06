6f0074f14a5397dc54847f50d1e5bf66a9c175fb
esjunior593 <esjunior593@gmail.com>
2025-02-05 21:09:23 -0500

    apiaa13

--- server.js
+++ server.js
@@ -29,10 +29,10 @@ db.connect(err => {
 
 // Función para extraer datos del OCR según diferentes formatos de comprobantes
 function extraerDatosOCR(text) {
-    const comprobanteRegex = /(?:Comprobante|Número de transacción|Código de transacción|Referencia|N°|No\.)[:\s]+(\d+)/i;
+    const comprobanteRegex = /(?:Comprobante\s*Nro\.?|Número de transacción|Código de transacción|Referencia|N°|No\.)[:\s]+(\d+)/i;
     const nombresRegex = /(?:Para:|Beneficiario:|Perteneciente a:|Nombre:|Titular Cuenta:)\s*([A-Za-z\s]+)/i;
     const montoRegex = /\$?\s?(\d+\.\d{2})/i; // Detecta $4.00 o 4.00
-    const fechaRegex = /(?:Fecha[:\s]+)(\d{1,2} [a-zA-Z]{3,} \d{4}|\d{2}\/\d{2}\/\d{4})/i;
+    const fechaRegex = /(?:Fecha[:\s]+)(\d{1,2} [a-zA-Z]{3,} \d{4}|\d{2}[-\/]\d{2}[-\/]\d{4})/i;
     const fechaHoraRegex = /(?:\w+,\s)?(\d{2} \w{3,}\. \d{4})\s*-\s*(\d{2}:\d{2})/i; // Detecta "Martes, 04 feb. 2025 - 14:02"
 
     let numero = text.match(comprobanteRegex) ? text.match(comprobanteRegex)[1] : " ";
