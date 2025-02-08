8144801bcc592ec602ab18b0b2a0638353a2f1a1
esjunior593 <esjunior593@gmail.com>
2025-02-07 22:34:44 -0500

    testy19

--- server.js
+++ server.js
@@ -290,7 +290,7 @@ else if (/Transferencia Enviada|COMPROBANTE DE TRANSFERENCIA/i.test(text) && /No
     
 
     //DEPOSITO BANCO DEL PACIFICO
-    else if (/Banco Del Pac[ií]fic/i.test(text) && /Comprobante De Transacci[oó]n/i.test(text)) {
+    else if (/Banco Del Pac[ií]fic/i.test(text) && /Comprobante De Transacci[oó]n/i.test(text) && /Secuencial/i.test(text)) {
         console.log("✅ Se detectó un comprobante de DEPÓSITO en Banco del Pacífico.");
         banco = "DEPÓSITO - BANCO DEL PACÍFICO";
     
@@ -304,13 +304,15 @@ else if (/Transferencia Enviada|COMPROBANTE DE TRANSFERENCIA/i.test(text) && /No
     
         // 🔹 Expresiones Regulares Mejoradas
         const numeroRegex = /transacci[oó]n[:\s]*([\d]+)/i;
+        const secuencialRegex = /secuencial[:\s]*([\d]+)/i;
         const nombresRegex = /nombre[:\s]*([a-z\s]+)/i;
         const montoRegex = /valor[:\s]*([\d,\.]+)/i;
         const fechaRegex = /fecha[:\s]*(\d{2}\/\d{2}\/\d{4})/i;
     
         // 📌 Extraer número de transacción
         let matchNumero = textoLimpio.match(numeroRegex);
-        numero = matchNumero ? matchNumero[1].trim() : "-";
+        let matchSecuencial = textoLimpio.match(secuencialRegex);
+        numero = matchNumero ? matchNumero[1].trim() : (matchSecuencial ? matchSecuencial[1].trim() : "-");
     
         // 📌 Extraer nombres
         let matchNombres = textoLimpio.match(nombresRegex);
@@ -326,6 +328,7 @@ else if (/Transferencia Enviada|COMPROBANTE DE TRANSFERENCIA/i.test(text) && /No
     
         console.log("📥 Datos extraídos:", { numero, nombres, monto, fecha, banco });
     }
+    
     
 
 
